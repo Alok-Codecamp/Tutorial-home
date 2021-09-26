8b69247ffd3521be541a1b3822066dd7d2f8c23c
Alok Kumar <alok61.bd@gmail.com>
2021-09-26 17:53:38 +0600

    order Summary css added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import logo from '../../logo.png'
+import './Header.css'
 const Header = () => {
     return (
-        <div>
-            <h1>this is header</h1>
-        </div>
+       <div className="header">
+            
+        <div className="menu-bar">
+            <div className="logo">
+                <img src={logo} alt="" />
+            </div>
+            <div className="nav-bar">
+                <nav>
+                    <a href="#">Subject List</a>
+                    <a href="#">For Student</a>
+                    <a href="#">For Parents</a>
+                    <a href="#">For Tutor</a>
+                    <a href="#">Pricing</a>
+                </nav>
+            </div>
+            </div>
+            <h4 className="budget">Total budget $30000 </h4>
+       </div>
     );
 };
 
--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './OrderSummary.css'
 const OrderSummary = (props) => {
     console.log(props.orders)
     let total=0;
@@ -9,17 +9,19 @@ const OrderSummary = (props) => {
     }
    }
     return (
-        <div>
-            <h1>this is order Summary</h1>
+        <div className="order-summary">
+            <h1>Order Summary</h1>
             <h3>Total Order: {props.orders.length}</h3>
-            <h3>Total price: {total}</h3>
-            {
-                props.orders.map(order=><div>
+            <h3>Total price: ${total}</h3>
+           <div className="tutor-info-container">
+           {
+                props.orders.map(order=><div className="order-info">
                 <p>Name: {order.tutor}</p>
                 <p>Subject:{order.subject}</p>
                 </div>)
                 
             }
+           </div>
 
         </div>
     );
