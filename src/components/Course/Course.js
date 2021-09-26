d8fc3cf0faa072a4ee3726e4a1f3c9690190e6ae
Alok Kumar <alok61.bd@gmail.com>
2021-09-26 16:15:22 +0600

    edit card css

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './Course.css'
 const Course = (props) => {
     // console.log(props)
-    const {subject, tutor, proffession, gender, age, img,email, price }=props.course;
+    const {subject, tutor, proffession, gender, age, img, email, price }=props.course;
     return (
         <div className="course">
-            <img src="" alt="" />
+
+            <img src={img} alt="" />
             <h3>Subject:{subject}</h3>
             <h4>Tutor: {tutor}</h4>
-            <p>age: {age}</p>
+            <p>Profession: {proffession}</p>
             <p>Email: {email}</p>
-            <p>Price: {price}</p>
+            <p>Price: ${price}</p>
             <button onClick={()=>props.handleOrder(props.course)}>buy Now</button>
             
         </div>
